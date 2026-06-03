34416b8daed21bd99a281b1807a985a0f4f0fcd1
erty2134 <olly.lansdown@gmail.com>
2026-06-03 17:40:40 +1000

    updated something idk

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from dotenv import load_dotenv, dotenv_values
 import os
@@ -7,11 +8,13 @@ import random
 
 load_dotenv()
 
-start: int = 1650
+#start: int = 1444
+start: int = 1524
 #end: int = 1745
-end: int = 1745
+end: int = 1927
 interval = 0.5
 randomness = 0.2
+output_file = open("out.txt", 'a')
 
 with requests.Session() as session:
     # enter compass
@@ -22,12 +25,30 @@ with requests.Session() as session:
     }
     session.get(url, headers=header, cookies=compass_cookies)
     
-    # get all classes
-    pattern = r'(?<=<div class="pageHero-title" data-test="hero-title" id="heroTitle">).*?(?=</div>)'
-    for i in range(start, end+1):
-        time.sleep(random.uniform(interval-randomness, interval+randomness))
-        url = f"https://maristeastwood-nsw.compass.education/Organise/Subjects/Subject.aspx?subjectId={i}"
-        response = session.get(url, headers=header, cookies=compass_cookies)
-        match = re.search(pattern, response.text)
-        #print(f"code: {response.status_code}, id: {i}")
-        print(f"code: {response.status_code}, class: {match.group()}, id: {i}")
+    # get all classes, (?<=).*?(?=)
+    subject_pattern = r'(?<=<div class="pageHero-title" data-test="hero-title" id="heroTitle">).*?(?=</div>)'
+    subject_code_pattern = r'(?<=<div class="pageHero-subtitle">).*?(?=</div>)'
+    try:
+        for i in range(start, end+1):
+            time.sleep(random.uniform(interval-randomness, interval+randomness))
+
+            url = f"https://maristeastwood-nsw.compass.education/Organise/Subjects/Subject.aspx?subjectId={i}"
+            response = session.get(url, headers=header, cookies=compass_cookies)
+
+            try:
+                subject_match = re.search(subject_pattern, response.text).group()
+            except AttributeError:
+                subject_match = "not found"
+            try:
+                subject_code_match = re.search(subject_code_pattern, response.text).group()
+            except AttributeError:
+                subject_code_match = "not found"
+
+            output = f"id: '{i}', https code: '{response.status_code}', class: '{subject_match}', subject code: '{subject_code_match}'"
+            print(output)
+            output_file.write(output+"\n")
+            output_file.flush()
+    except KeyboardInterrupt:
+        pass
+    finally:
+         output_file.close()
